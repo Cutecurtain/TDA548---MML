bdfb9410bbd7b776957abf390ec81121c17420ff
LatteG <gustav.lahti@gmail.com>
2016-08-30 16:58:11 +0200

    Add files via upload

--- Nim.java
+++ Nim.java
@@ -1,13 +1,11 @@
 package assignments.product;
 
-import jdk.internal.util.xml.impl.Input;
-
+import java.util.Random;
 import java.util.*;
 
 import static java.lang.System.*;
 
 /*
-
  * Nim Game
  * See https://en.wikipedia.org/wiki/Nim
  *
@@ -19,55 +17,113 @@ public class Nim {
         new Nim().program();
     }
 
+    int AITurn(int pile, int minTake, int maxTake){
+        // Algorithm for optimal(?) play by the AI
+        return minTake + (pile % (minTake + maxTake));
+    }
+
+    int playerTurn(int pile, int minTake, int maxTake){
+        int take = 0;
+        Boolean success = false;
+
+        Scanner input = new Scanner(in);
+
+        while(!success)
+            try {
+                while ((take = input.nextInt()) < minTake || take > maxTake)
+                    out.print("Illegal amount! Take coins ( " + minTake + " - " + maxTake + " ) > ");
+                success = true;
+            }
+
+            catch(InputMismatchException e){
+                input.nextLine();
+                out.print("Not an integer! Take coins ( " + minTake + " - " + maxTake + " ) > ");
+            }
+
+        return take;
+    }
+
+    Boolean restart(){
+        int again = -1;
+        Boolean success = false;
+
+        Scanner input = new Scanner(in);
+
+        while(!success) {
+            try {
+                while ((again = input.nextInt()) < 0 || again > 1)
+                    out.print("Illegal input! Do you want to play again? ( 0 = no, 1 = yes) > ");
+                success = true;
+            } catch (InputMismatchException e) {
+                input.nextLine();
+                out.print("Not an integer! Do you want to play again? ( 0 = no, 1 = yes) > ");
+            }
+        }
+
+        if(again == 1)
+            return true;
+        return false;
+    }
+
     void program() {
-        Scanner inp = new Scanner(in);      //Create scanner object to read
-        Random rand = new Random();         //Create random object to generate random integers for gameplay
+        boolean play = true;
+        while (play) {
+            int pile = 13;
+            int maxTake = 3;
+            int minTake = 1;
+            int take;
+            Boolean turn = true; // true = AI, false = human
 
-        while (true) {
-            //Start
-            int coins = rand.nextInt(40) + 10;              //Randomize the initial number of coins
-            int maxdraw = rand.nextInt(coins / 5 + 1) + 2;  //Randomize the allowed number of coins to be taken
-            int player_input = 0;                           //Variable to store player input integer
-            int other_move = 0;                             //The computer's "input"
-            int error_count = 0;                            //To prevent total fuck up
-            boolean try_success = false;                    //Switches to true if inp.nextInt() is successful
+            out.println("Welcome to NIM");
 
-            while (coins > 0) { //Main gameplay loop
-                out.print("There are " + coins + " coins in the pile, take 1 to " + maxdraw + " coins > ");
+            while (pile > 0) {
+                if (pile == 1)
+                    out.println("There's 1 coin in the pile");
+                else
+                    out.println("There's " + pile + " coins in the pile");
 
-                error_count = 0;
-                //Input, loop until a valid input is given (or until 5 errors occurr, but should never happen)
-                while (!try_success && error_count < 5) {
-                    try_success = false;
-                    try {
-                        player_input = inp.nextInt();
-                        while (!(player_input > 0 && player_input < maxdraw + 1 && coins >= player_input)) {
-                            out.print("Input is OOB (1 to " + maxdraw +") > ");
-                            player_input = inp.nextInt();
-                            try_success = true;
-                        }
-                    } catch (InputMismatchException e) {
-                        error_count++;
-                        out.print("Non-integer input > ");
-                        inp.nextLine();
-                    }
-                }
-                coins -= player_input; //Player done, check win-con
-                if (coins <= 0) {
-                    out.print("Player wins!\n\n\n\n");
-                    break;
+                turn = !turn;
+
+                // AI's turn
+                if (turn) {
+                    // AI take function call
+                    take = AITurn(pile, minTake, maxTake);
+
+                    pile -= take;
+                    if (take == 1)
+                        out.println("AI took 1 coin");
+                    else
+                        out.println("AI took " + take + " coins");
                 }
 
-                other_move = coins % maxdraw + 1;   //Always tries to do the optimal move
-                if (other_move == 0)                //If the optimal is to not take any, randomize the action
-                    other_move = rand.nextInt(maxdraw) + 1;
-                out.println("The computer takes " + other_move + " coins");
-                coins -= other_move % (maxdraw + 1);    //Computer done,
-                if (coins <= 0) {
-                    out.print("Computer wins!\n\n\n\n");
-                    break;
+                // Player's turn
+                else {
+                    out.print("Take coins > ");
+
+                    // Input -- makes sure the amount taken is not too big/small
+                /*while((take = input.nextInt()) < minTake || take > maxTake)
+                    out.print("Illegal amount! Take coins ( " + minTake + " - " + maxTake + " ) > ");*/
+
+                    take = playerTurn(pile, minTake, maxTake);
+
+                    pile -= take;
+                    if (take == 1)
+                        out.println("Player took 1 coin");
+                    else
+                        out.println("Player took " + take + " coins");
                 }
             }
+
+            out.println();
+            out.print("The winner is the ");
+            if (turn)
+                out.println("AI!");
+            else
+                out.println("player!");
+            out.println();
+
+            out.print("Do you want to play again? (0 = no, 1 = yes) > ");
+            play = restart();
         }
     }
 }
